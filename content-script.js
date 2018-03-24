f8a6b943c2347e81810d176bbca607bf4687bf9b
ebinan92 <ebinan92@outlook.jp>
2018-03-24 22:40:38 +0900

    key変更機能追加

--- content-script.js
+++ content-script.js
@@ -1,21 +1,28 @@
 var baseurl = "https://ejje.weblio.jp/content/";
 
-//ctrl,shift,s
-var map = {16: false, 17: false, 83: false};
+// ctrl,shift,s
+const key1 = 16, key2 = 17, key3 = 83;
+
+var map = {[key1]: false, [key2]: false, [key3]: false};
 
 $(document).keydown(function(e) {
     if (e.keyCode in map) {
         map[e.keyCode] = true;
-        if (map[16] && map[17] && map[83]) {
+        console.log(e.keyCode);
+        if (map[key1] && map[key2] && map[key3]) {
             // FIRE EVENT
-            if (window.getSelection()) {
-                text = window.getSelection().toString();
+            text = window.getSelection().toString();
+            if (text) {
                 chrome.runtime.sendMessage(
                     baseurl + text,
                     function (response) {
-                        console.log(response);
+                        console.log(response)
                     }
                 );
+                //initialization
+                for (var key in map) {
+                    map[key] = false;
+                }
             }
         }
     }
